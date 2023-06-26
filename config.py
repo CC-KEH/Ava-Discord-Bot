730bead319ebdb3de132be0124d621930dda2248
CC-KEH <arbashhussain08@gmail.com>
2023-06-26 22:54:35 +0530

    hmm

--- config.py
+++
@@ -1,6 +0,0 @@
-#appid: 1122565355143958530
-#publickey: dee134e1d66d6bca2e2e13cdfbc45b655ad24a0da01b68fb3b91c8c50ac483c0
-#MTEyMjU2NTM1NTE0Mzk1ODUzMA.GMiwxu.ZQXgjpIZ33oqMals8cabpLRP2qyUcm82nZLd5Y
-#https://discord.com/api/oauth2/authorize?client_id=1122565355143958530&permissions=4398083214336&scope=bot
-BARD_API = 'XAjXGRoq7sRhUOV8xmtQc4ZMB7l-anG2Dboe9EdO0FRpjM1WUdUnC3T_s_rSfIYL8wxjDw.'
-TOKEN = 'MTEyMjU2NTM1NTE0Mzk1ODUzMA.GTzYw2.anY7HfFl6jJbNsXJgac525YThIe4dgqTGyZhhU'
